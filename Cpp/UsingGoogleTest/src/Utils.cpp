ffa5ddd9068327d48aed0cf7ade643aa697e8afe
jai1 <jai1@yahoo-inc.com>
2016-03-28 17:21:26 -0700

    Added code

---
+++ Cpp/UsingGoogleTest/src/Utils.cpp
@@ -0,0 +1,29 @@
+/*
+ * Utils.cpp
+ *
+ *  Created on: Mar 28, 2016
+ *      Author: jai1
+ */
+
+#include <Utils>
+
+namespace cms {
+
+Utils::Utils() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Utils::~Utils() {
+	// TODO Auto-generated destructor stub
+}
+
+static long Utils::square(int x) {
+	return x * x;
+}
+
+static long Utils::cube(int x) {
+	return x * x * x;
+}
+
+} /* namespace cms */
