903f9a401e9c0dce95d428da0af8617d7f9e25ac
jai1 <jai1@yahoo-inc.com>
2016-07-03 01:54:10 -0700

    Reorganized

--- CTCI/src/main/java/org/ctci/learn/CustomException.java
+++
@@ -1,7 +0,0 @@
-package org.ctci.learn;
-
-public class CustomException extends Exception {
-	public CustomException(String message) {
-        super(message);
-    }
-}
--- CTCI/src/main/java/org/ctci/utils/SortingOrder.java
+++
@@ -1,3 +0,0 @@
-package org.ctci.utils;
-
-public enum SortingOrder { ASCENDING, DESCENDING }
